6b8817b9da19a49981568c6c97e4ef523d045e1b
赵文刚 <770586536@qq.com>
2019-09-30 16:41:10 +0800

    init commit

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+#引入HttpResponse响应类
+from django.shortcuts import HttpResponse
+#定义welcome函数
+def welcome(request):
+    #返回响应对象，参数为字符串‘hello django!’
+    #参数会显示在网页中
+    return HttpResponse('hello django!')
